34878d78388514c808b14328577c1fb7c2e1428e
Saloni Vithalani <skvithalani@yahoo.com>
2019-05-17 14:30:15 +0530

    #000: Saloni | Puneet : Make lib from  scalajs, publish In npm

--- src/main/scala/HelloWorldLib.scala
+++ src/main/scala/HelloWorldLib.scala
@@ -1,6 +1,7 @@
-import scala.scalajs.js.annotation.JSExport
+import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 
-class HelloWorldLib {
+@JSExportTopLevel("HelloWorldLib")
+object HelloWorldLib {
   @JSExport
   def square(value: Int): Int = value * value
 }
